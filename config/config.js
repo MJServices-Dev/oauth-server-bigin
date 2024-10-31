fb603132dc8737684e942e57bd3b11b486417724
vizzscript <script.vizz@gmail.com>
2024-10-31 16:20:09 +0530

    update config.js

--- config/config.js
+++ config/config.js
@@ -3,8 +3,8 @@ require('dotenv').config(); // Load environment variables from .env file
 const config = {
     PORT: process.env.PORT || 3000, // Default port
     MONGO_URI: process.env.MONGO_URI || 'mongodb://localhost:27017/mydatabase', // MongoDB connection string
-    GOOGLE_CLIENT_ID: process.env.CLIENT_ID, // Google Client ID from .env
-    GOOGLE_CLIENT_SECRET: process.env.CLIENT_SECRET, // Google Client Secret from .env
+    CLIENT_ID: process.env.CLIENT_ID, // Google Client ID from .env
+    CLIENT_SECRET: process.env.CLIENT_SECRET, // Google Client Secret from .env
     REDIRECT_URI: process.env.REDIRECT_URI || 'http://localhost:3000/auth/google/callback', // Redirect URI
     SESSION_SECRET: process.env.SESSION_SECRET || 'mysecret', // Secret for session management
 };
